b5ee21b2b595b042e52247c2bb319e6c139c0c3c
Siavash <siamhb7@protonmail.com>
2023-02-13 18:50:49 +0330

    work on the footer component and its tests.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import BookCart from "src/components/bookCard/bookCard";
 import BookIntroducer from "src/components/bookIntroducer/bookIntroducer";
+import Footer from "src/components/footer/footer";
 import Navbar from "src/components/navbar/navbar";
 import { book1, book2, book3, book4 } from "src/shared/fakeBooks";
 
@@ -11,32 +12,43 @@ export default function Home() {
         <title>Booker Home Page</title>
         <meta name="description" content="Booker Home Page" />
       </Head>
-      <Navbar />
-      {/* <div style={{ display: "flex" }}>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          minHeight: "100vh",
+        }}
+      >
+        <Navbar />
+        {/* <div style={{ display: "flex" }}>
         <BookCart {...book1} />
         <BookCart {...book2} />
         <BookCart {...book3} />
         <BookCart {...book4} />
       </div> */}
-      <div style={{ width: "100%" }}>
-        <BookIntroducer
-          introducingName="Popular Books"
-          hrefToAllBooks="/"
-          books={[
-            book1,
-            book2,
-            book3,
-            book4,
-            book1,
-            book2,
-            book3,
-            book4,
-            book1,
-            book2,
-            book3,
-            book4,
-          ]}
-        />
+        <div style={{ width: "100%" }}>
+          <BookIntroducer
+            introducingName="Popular Books"
+            hrefToAllBooks="/"
+            books={[
+              book1,
+              book2,
+              book3,
+              book4,
+              book1,
+              book2,
+              book3,
+              book4,
+              book1,
+              book2,
+              book3,
+              book4,
+            ]}
+          />
+        </div>
+        <Footer />
       </div>
     </>
   );
