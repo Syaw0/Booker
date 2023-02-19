1f62d82f270c4e7fce4d36114a213ad3d60f3f2f
Siavash <siamhb7@protonmail.com>
2023-02-19 16:17:20 +0330

    work on the user dash cart page serverside props .

---
+++ server/graphql/utils/getCartData.ts
@@ -0,0 +1,32 @@
+import getFilteredBooks from "../../../db/utils/getFilteredBooks";
+import makeQueryForWishList from "./makeQueryForWishlist";
+
+const getCartData = async (cart: string[]) => {
+  try {
+    let cartData: any = {};
+    cart.forEach((c) => {
+      if (cartData[c] != null) {
+        cartData[c].num += 1;
+      } else {
+        cartData[c] = { num: 1 };
+      }
+    });
+    const query = makeQueryForWishList(Object.keys(cartData));
+    const books = await getFilteredBooks(query, 0, 100000);
+    if (books.status) {
+      books.data.forEach((book: any) => {
+        if (cartData[book.bookId] != null) {
+          cartData[book.bookId] = { ...cartData[book.bookId], ...book };
+        }
+      });
+    } else {
+      return { status: false, msg: "Error In Get Books!" };
+    }
+
+    return { status: true, msg: "", data: Object.values(cartData) };
+  } catch (err) {
+    return { status: false, msg: "Error During Get Cart Data!" };
+  }
+};
+
+export default getCartData;
--- src/pages/user/cart/index.tsx
+++ src/pages/user/cart/index.tsx
@@ -1,8 +1,11 @@
+import checkSession from "db/utils/checkSession";
+import getUserById from "db/utils/getUserById";
 import { GetServerSideProps, GetServerSidePropsResult } from "next";
 import Head from "next/head";
 import { Provider } from "react-redux";
+import calculatePrices from "server/graphql/utils/calculatePrices";
+import getCartData from "server/graphql/utils/getCartData";
 import UserCart from "src/components/pageComponents/userCart/userCart";
-import fakeUserCartPageData from "src/shared/fakeUserCartPageData";
 import navItems from "src/shared/userDashNavItems";
 import makeStore from "src/store/userCart/userCart";
 
@@ -19,12 +22,57 @@ const UserCartPage = (props: UserCartPagePropsTypes) => {
     </>
   );
 };
-const getServerSideProps: GetServerSideProps = async (): Promise<
-  GetServerSidePropsResult<UserCartPagePropsTypes>
-> => {
+const getServerSideProps: GetServerSideProps = async ({
+  req,
+}): Promise<GetServerSidePropsResult<UserCartPagePropsTypes>> => {
+  const props: UserCartPagePropsTypes = {
+    isLogin: false,
+    user: {
+      cartNumber: "",
+      email: "",
+      profileUrl: "",
+      userId: "",
+      wishlist: [],
+      addresses: [],
+      cart: [],
+      orders: [],
+    },
+    actionType: "userCart",
+    menuItems: [],
+    navbarItems: [],
+    addresses: [],
+    books: [],
+    priceSummary: {
+      shipping: "",
+      subTotal: "",
+      tax: "",
+      total: "",
+    },
+  };
+
+  const checkSessionResult = await checkSession(req.cookies);
+  if (checkSessionResult.status && checkSessionResult.data != null) {
+    const user = await getUserById(checkSessionResult.data);
+    if (user.status && user.data != null) {
+      props.user = user.data;
+      props.isLogin = true;
+      console.log(user.data.cart);
+      if (user.data.cart.length != 0) {
+        const cart = await getCartData(user.data.cart);
+        if (cart.status) {
+          props.books = cart.data as BookCartCardPropsType[];
+          let summary = calculatePrices(cart.data);
+          props.priceSummary = summary;
+        }
+      }
+    } else {
+      return { redirect: { destination: "/500", permanent: false } };
+    }
+  }
+
   return {
     props: {
-      ...fakeUserCartPageData,
+      ...props,
     },
   };
 };
